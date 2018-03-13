0208c6aeeb070a42bc1eebf179cdae17538cc23e
steveinc911 <stevesales@gmail.com>
2018-03-13 07:59:18 +0200

    Signed-off-by: steveinc911 <stevesales@gmail.com>

---
+++ notesnode/app.js
@@ -0,0 +1,9 @@
+console.log("stating app.js");
+
+const notes = require('./notes.js');
+const _ = require('lodash');
+
+console.log(_.isString(true));
+console.log(_.isString('Andrew'));
+
+console.log('Result:', notes.add(9, -2));
